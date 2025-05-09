c550b61e04dd5e40f83290e817eb5462865d976a
Rodrigo Souza <rosantanasouza@gmail.com>
2025-05-09 16:48:34 -0300

    Adicionando o arquivo SistemaBancario.java

---
+++ src/SistemaBancario.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class SistemaBancario {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String nomeCliente = "John Doe";
+        String tipoConta = "Corrente";
+        double saldo = 2500.00;
+        double valorSaque;
+        double valorDeposito;
+
+        System.out.println("****iNFORMAÇÃO CLIENTE********");
+        System.out.println("CLIENTE:        " + nomeCliente);
+        System.out.println("TIPO CONTA:     " + tipoConta);
+        System.out.println("SALDO:          " + saldo);
+        System.out.println("******************************");
+
+        int opcao = 0;
+        while (opcao != 4) {
+            System.out.println("""                
+                **********MENU****************
+                DIGITE A OPÇÃO DA OPERAÇÃO:
+                1 - Consultar saldo
+                2 - Realizar saque
+                3 - Depositar valor
+                4 - Sair
+                ******************************
+                """);
+            opcao = scan.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Saldo atual: " + saldo);
+                    break;
+
+                case 2:
+                    System.out.println("Digite o valor que deseja sacar:");
+                    valorSaque = scan.nextDouble();
+                    if (valorSaque > saldo) {
+                        System.out.println("Não é possível sacar um valor maior do que o saldo.\n" +
+                                           "Seu saldo é: " + saldo + "\n");
+                    } else {
+                        saldo = saldo - valorSaque;
+                        System.out.println("Saque realizado com sucesso.");
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Digite o valor que deseja depositar:");
+                    valorDeposito = scan.nextDouble();
+                    saldo = saldo + valorDeposito;
+                    break;
+            }
+        }
+        System.out.println("Conta desconectada com segurança.");
+    }
+}
