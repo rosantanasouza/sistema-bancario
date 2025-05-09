93546a5acf49d6ffbc8abccb1a6a4b0a590b7a61
Rodrigo Souza <rosantanasouza@gmail.com>
2025-05-09 18:12:34 -0300

    Ajustes no código.

--- src/SistemaBancario.java
+++ src/SistemaBancario.java
@@ -6,8 +6,6 @@ public class SistemaBancario {
         String nomeCliente = "John Doe";
         String tipoConta = "Corrente";
         double saldo = 2500.00;
-        double valorSaque;
-        double valorDeposito;
 
         System.out.println("****iNFORMAÇÃO CLIENTE********");
         System.out.println("CLIENTE:        " + nomeCliente);
@@ -18,40 +16,50 @@ public class SistemaBancario {
         int opcao = 0;
         while (opcao != 4) {
             System.out.println("""                
-                **********MENU****************
-                DIGITE A OPÇÃO DA OPERAÇÃO:
-                1 - Consultar saldo
-                2 - Realizar saque
-                3 - Depositar valor
-                4 - Sair
-                ******************************
-                """);
+                    **********MENU****************
+                    DIGITE A OPÇÃO DA OPERAÇÃO:
+                    1 - Consultar saldo
+                    2 - Realizar saque
+                    3 - Depositar valor
+                    4 - Sair
+                    ******************************
+                    """);
             opcao = scan.nextInt();
 
             switch (opcao) {
                 case 1:
-                    System.out.println("Saldo atual: " + saldo);
+                    System.out.println("--> Saldo atual: " + saldo + "\n");
                     break;
 
                 case 2:
                     System.out.println("Digite o valor que deseja sacar:");
-                    valorSaque = scan.nextDouble();
+                    double valorSaque = scan.nextDouble();
                     if (valorSaque > saldo) {
                         System.out.println("Não é possível sacar um valor maior do que o saldo.\n" +
-                                           "Seu saldo é: " + saldo + "\n");
+                                "Seu saldo é: " + saldo + "\n");
                     } else {
                         saldo = saldo - valorSaque;
-                        System.out.println("Saque realizado com sucesso.");
+                        System.out.println("--> Saque realizado com sucesso.");
+                        System.out.println("Saldo atual: " + saldo + "\n");
                     }
                     break;
 
                 case 3:
                     System.out.println("Digite o valor que deseja depositar:");
-                    valorDeposito = scan.nextDouble();
+                    double valorDeposito = scan.nextDouble();
                     saldo = saldo + valorDeposito;
+                    System.out.println("--> Deposito realizado com sucesso.");
+                    System.out.println("Saldo atual: " + saldo + "\n");
+                    break;
+
+                case 4:
+                    System.out.println("Conta bancária desconectada com segurança.");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida. Por favor, digite uma opção válida.\n");
                     break;
             }
         }
-        System.out.println("Conta desconectada com segurança.");
     }
 }
